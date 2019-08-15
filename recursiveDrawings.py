69d3f1fb58fcf2f4f75803a3d6a4a050b538639b
Jennifer Pham <spis19ba@its-cseb230-21.ucsd.edu>
2019-08-15 13:57:29 -0700

    ADDED A TREE

--- recursiveDrawings.py
+++ recursiveDrawings.py
@@ -14,6 +14,49 @@ def spiral(initialLength, angle, multiplier):
 
     spiral(initialLength * multiplier, angle, multiplier)
 
+
+def tree(trunkLength, height):
+    if height == 1:
+        turtle.forward(trunkLength)
+        turtle.right(45)
+        turtle.forward(trunkLength // 2)
+        turtle.backward(trunkLength // 2)
+        turtle.left(90)
+        turtle.forward(trunkLength // 2)
+        turtle.backward(trunkLength // 2)
+        turtle.right(45)
+        #turtle.backward(trunkLength)
+
+    else:
+        turtle.forward(trunkLength)
+        turtle.right(45)
+        #turtle.forward(trunkLength // 2)
+        tree(trunkLength//2, height-1)
+        turtle.backward(trunkLength // 2)
+        turtle.left(90)
+        #turtle.forward(trunkLength // 2)
+        tree(trunkLength//2, height-1)
+        turtle.backward(trunkLength // 2)
+        turtle.right(45)
+        #turtle.backward(trunkLength)
+        #turtle.forward(trunkLength)
+        #turtle.right(45)
+        #print("turtle moving right!" + str(turtle.heading()))
+        #tree(trunkLength * .5, height-1)
+        #print("Turtle going back!" + str(turtle.heading()))
+        #turtle.left(45)
+        #turtle.backward(trunkLength)
+        #turtle.left(90)
+        #print("turtle moving lef!" + str(turtle.heading()))
+        #t2ree(trunkLength*.5, height-1)
+        #print("Turtle going back 2.0!" + str(turtle.heading()))
+        #turtle.right(135)
+        #turtle.backward(trunkLength)
+        #turtle.right(45)
+        input("hi")
 #Test case
-spiral(1, -45, 1.1)
+#spiral(1, -45, 1.1)
+turtle.speed(1)
+turtle.left(90)
+tree(200, 4)
 
